e97ab36b9235d5d67f25f1d29ff61439da2860d8
Mikkel Vilstrup <Mikkel@Vilstrup.dk>
2014-05-19 10:33:59 +0200

    Made some simple tests
    I just created some simple tests for teh question class I created yesterday.. Futhermore I deleted the admin app since this is not going to be used in practice.

---
+++ root/tests/test_models.py
@@ -0,0 +1,25 @@
+from django.test 					import TestCase
+from django.contrib.auth.models 	import User
+from django.core.urlresolvers 		import reverse
+from root.models 					import Question
+
+
+class QuestionTests(TestCase):
+
+	def setUp(self):
+		self.user = User.objects.create(username="test")
+
+	def create_question(question="Test Question", answer="42", subject="S"):
+		return Question.objects.create(
+			question 	= question,
+			answer 		= answer,
+			subject 	= subject
+		)
+
+	def test_question_creation(self):
+		question = self.create_question()
+
+		self.assertTrue(isinstance(question, Question))
+		self.assertEquals(question.__unicode__(), question.question)
+
+		
