cc90bb1417404147e953b81815407bbb2718cc1f
Mikkel Vilstrup <Mikkel@Vilstrup.dk>
2014-05-23 13:18:14 +0200

    Got Quill working and updated requirements
    I got quill working, the form in itself is not doing anything, but quill is being displayed nicely.
    
    I also updated the Requirements, with packages we are most likely going to use in the project

--- leapkit/urls.py
+++ leapkit/urls.py
@@ -6,7 +6,7 @@ from root import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    
+
 	#URLs used when not signed in (views and functionality stored in root app). They are accessed here, to make the URL prettier
     url(r'^$', views.HomepageView.as_view(), name='home'),
    	url(r'student-info/$', views.StudentInfoView.as_view(), name='student_info'),
@@ -15,12 +15,12 @@ urlpatterns = patterns('',
     url(r'get-started/$', views.GetStartedView.as_view(), name='getting_started'),
     url(r'about-leapkit/$', views.AboutLeapkitView.as_view(), name='about_leapkit'),
     url(r'FAQ/$', views.FAQView.as_view(), name='FAQ'),
-    url(r'contact-lepkit/$', views.ContactLeapView.as_view(), name='contact_leapkit'),
+    url(r'contact-leapkit/$', views.ContactLeapView.as_view(), name='contact_leapkit'),
     url(r'signin/$', views.SignInView.as_view(), name='sign_in'),
     url(r'student_signup/$', views.StudentSignUpView.as_view(), name='student_sign_up'),
     url(r'company_signup/$', views.CompanySignUpView.as_view(), name='company_sign_up'),
     url(r'signup/$', views.SignUpView.as_view(), name='sign_up'),
-    
+
     #All other URLs are stored in their respective app.
     url(r'^students/', include('students.urls', namespace="students")),
     url(r'^companies/', include('companies.urls', namespace="companies")),
